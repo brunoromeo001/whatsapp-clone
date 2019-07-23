4c47d16f08315dc28f0a0501040e9fd48a9c3239
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-07-23 18:16:59 -0300

    Clique no Menu Anexar

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,8 @@ class WhatsAppController{
         
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
+
     }
 
     loadElements(){
@@ -53,7 +55,7 @@ class WhatsAppController{
 
             for (let name in styles){
 
-                this.style[name]= style[name];
+                this.style[name]= styles[name];
             }
 
             return this;
@@ -84,5 +86,136 @@ class WhatsAppController{
 
             return this.classList.contains(name);
         }
+
+        HTMLFontElement.prototype.getForm = function (){
+
+            return new FormData(this);
+        }
+        
+        HTMLFontElement.prototype.toJSON = function (){
+
+            let json = {};
+
+            this.getForm().forEach((value, key)=>{
+
+                json[key] = value;
+
+            });
+
+            return json;
+        }
+
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                
+                this.el.panelEditProfile.addClass('open');
+
+            }, 300);
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+            
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                
+                this.el.panelAddContact.addClass('open');
+            
+            }, 300);
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+        
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e=>{
+
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+            if (e.key === 'Enter'){
+
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item =>{
+
+            item.on('click', e=>{
+
+                this.el.home.hide();
+                this.el.main.css({
+
+                    display:'flex'
+                });
+
+            });
+        });
+
+        this.el.btnAttach.on('click', e=>{
+
+            e.stopPropagation();
+
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+
+        this.el.btnAttachPhoto.on('click', e=>{
+
+            console.log('photo');
+        });
+
+        this.el.btnAttachCamera.on('click', e=>{
+
+            console.log('camera');
+        });
+
+        this.el.btnAttachDocument.on('click', e=>{
+
+            console.log('document');
+        });
+
+        this.el.btnAttachContact.on('click', e=>{
+
+            console.log('Contact');
+        });
+    }
+
+    closeMenuAttach(e){
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+       
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 }
